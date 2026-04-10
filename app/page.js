634a4f79760d8db2d6a1440b887fbe7fc966264b
Gabriel Santiago <61061170+gabrielUpON@users.noreply.github.com>
2026-04-10 17:27:28 +0000

    update

--- app/page.js
+++ app/page.js
@@ -33,78 +33,84 @@ export default function Home() {
 
         <Modal isOpen={open} onClose={() => setOpen(false)}/>
 
-        <div className={styles.container}>
-          <div className={styles.apresentacaoWrapper}>
-            <div className={styles.apresentacao}>
-              <span>
-                Sua plataforma completa
-              </span>
+        <div className={styles.container1}>
+          <div className={styles.apresentacao}>
+            <span>Sua plataforma completa</span>
+            <span>para otimizar suas vendas</span>
 
-              <span>
-                para otimizar suas vendas
-              </span>
-
-              <p>
-                Analise, gerencie e otimize de forma inteligente seus clientes, produtos e a força da sua equipe.
-              </p>
-            </div>
-
-            <button className={styles.botaocentral} onClick={() => setOpen(true)}>
-              Comece Agora !
-            </button>
-
-            <div className={styles.metricas}>
-
-              <div className={styles.specMetricas}>
-                <Image src="/people-svgrepo-com.svg" width={23} height={23} alt="clientes"/>
-                <p>1000+ clientes</p>
-              </div>
-
-              <div className={styles.specMetricas}>
-                <Image src="/box-1-svgrepo-com.svg" width={23} height={23} alt="produtos"/>
-                <p>5000+ produtos</p>
-              </div>
-
-              <div className={styles.specMetricas}>
-                <Image src="/star-shape-1-svgrepo-com.svg" width={23} height={23} alt="IA"/>
-                <p>IA personalizada</p>
-              </div>
-
-              <div className={styles.specMetricas}>
-                <Image src="/pie-chart-1-svgrepo-com.svg" width={23} height={23} alt="Relatórios"/>
-                <p>Relatórios gerenciais</p>
-              </div>
-
-            </div>
-
-            <Image
-              className={styles.grafico}
-              src="/line-graph-svgrepo-com.svg"
-              alt="Radar"
-              width={150}   // ou valor responsivo
-              height={150}
-              style={{ objectFit: 'contain' }}
-            />
+            <p>
+              Analise, gerencie e otimize de forma inteligente seus clientes, produtos e a força da sua equipe.
+            </p>
           </div>
 
-          <article>
-            <h2>Analise vendas em produndidade</h2>
-            <p>Analise, crie, envie pedidos de forma facil e inteligente.Acompanhe o andamento até a prateleira.</p>
-          </article>
-
-          <article>
-            <h2>Genrencie seus clientes</h2>
-            <p>O que ele comprou ou deixou de comprar, relatório financeiro, histórico e relação.</p>
-          </article>
-
-          <article>
-            <h2>Tire dúvidas com a IA</h2>
-            <p>Uma IA treinada e alimentada com ativos e históricos da Vet & Agro capaz de lhe auxiliar de forma inteligente!</p>
-          </article>
-
+          <div className={styles.graficoWrapper}>
+            <Image
+              className={styles.grafico1}
+              src="/line-graph-svgrepo-com.svg"
+              alt="Radar"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
-        
+
+        <div className={styles.container2}>
+          <button className={styles.botaocentral} onClick={() => setOpen(true)}>
+            Comece Agora !
+          </button>
+
+          <div className={styles.metricas}>
+
+            <div className={styles.specMetricas}>
+              <Image src="/people-svgrepo-com.svg" width={23} height={23} alt="clientes"/>
+              <p>1000+ clientes</p>
+            </div>
+
+            <div className={styles.specMetricas}>
+              <Image src="/box-1-svgrepo-com.svg" width={23} height={23} alt="produtos"/>
+              <p>5000+ produtos</p>
+            </div>
+
+            <div className={styles.specMetricas}>
+              <Image src="/star-shape-1-svgrepo-com.svg" width={23} height={23} alt="IA"/>
+              <p>IA personalizada</p>
+            </div>
+
+            <div className={styles.specMetricas}>
+              <Image src="/pie-chart-1-svgrepo-com.svg" width={23} height={23} alt="Relatórios"/>
+              <p>Relatórios gerenciais</p>
+            </div>
+
+          </div>
+
+          <Image
+            className={styles.grafico2}
+            src="/line-graph-svgrepo-com.svg"
+            alt="Radar"
+            loading="eager"
+            width={150}   // ou valor responsivo
+            height={150}
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+
+        <article>
+          <h2>Analise vendas em produndidade</h2>
+          <p>Analise, crie, envie pedidos de forma facil e inteligente.Acompanhe o andamento até a prateleira.</p>
+        </article>
+
+        <article>
+          <h2>Genrencie seus clientes</h2>
+          <p>O que ele comprou ou deixou de comprar, relatório financeiro, histórico e relação.</p>
+        </article>
+
+        <article>
+          <h2>Tire dúvidas com a IA</h2>
+          <p>Uma IA treinada e alimentada com ativos e históricos da Vet & Agro capaz de lhe auxiliar de forma inteligente!</p>
+        </article>
+
       </div>
+
       <div className={styles.footer}>
 
         <p>© 2026 Vet & Agro Ltda. Todos os direitos reservados.</p>
