ef1d1e106e21ebf97aa03a09c8eb9fdd749a8fa6
Gabriel Santiago <61061170+gabrielUpON@users.noreply.github.com>
2026-04-17 11:43:15 +0000

    teste

--- components/home/modal.jsx
+++ components/home/modal.jsx
@@ -2,11 +2,29 @@
 
 import styles from "./Modal.module.css";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Modal({ isOpen, onClose }) {
   const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const USER = "admin@vetsforce.com";
+  const PASSWORD = "Vet753951";
 
   if (!isOpen) return null;
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email === USER && password === PASSWORD) {
+      router.push("/blog");
+    } else {
+      alert("E-mail ou senha inválidos");
+    }
+  }
 
   return (
     <div className={styles.main}>
@@ -14,19 +32,29 @@ export default function Modal({ isOpen, onClose }) {
 
         <button className={styles.closer} onClick={onClose}>X</button>
 
-        <form onSubmit={(e) => e.preventDefault()}>
+        <form onSubmit={handleLogin}>
           <h2>Bem-vindo!</h2>
           <h3>Faça login na sua conta VetsForce</h3>
 
-          <label htmlFor="email"><b>E-mail</b></label>
-          <input type="text" placeholder="seuemail@exemplo.com" name="email" required />
+          <label><b>E-mail</b></label>
+          <input 
+            type="text" 
+            placeholder="seuemail@exemplo.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required 
+          />
 
-          <label htmlFor="psw"><b>Senha</b></label>
+          <label><b>Senha</b></label>
           <div className={styles.showPsw}>
-            <input type="password" placeholder="••••••••" name="psw" required />
-            <button type="button" onClick={() => setShowPassword(!showPassword)}>
-              👁
-            </button>
+            <input 
+              type={showPassword ? "text" : "password"}
+              placeholder="••••••••"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required 
+            />
+            <p onClick={() => setShowPassword(!showPassword)}>Mostrar senha</p>
           </div>
               
           <button type="submit">Acessar</button>
